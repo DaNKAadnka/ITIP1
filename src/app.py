794fb6b5fe2e4a4cfe19b3c522865a9462ce8360
DaNKAadnka <danka2202@yandex.ru>
2023-09-18 11:27:45 +0300

    Ignoring files

---
+++ src/app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from functional import formation
+import os
+
+app = Flask(__name__)
+
+eport = 5000
+ehost = '127.0.0.1'
+ename = 'Danil'
+
+def fact (n) :
+    res = 1
+    for i in range (2, n+1) :
+        res *= i
+    return res
+
+@app.route('/')
+def hello() -> str:
+    return formation('Hello, {}', ename)
+
+if __name__ == '__main__':
+    if 'EPORT' in os.environ:
+        eport = os.environ['EPORT']
+        print(eport)
+    if 'EHOST' in os.environ:
+        ehost = os.environ['EHOST']
+        print(ehost)
+    if 'ENAME' in os.environ:
+        ename = os.environ['ENAME']
+        print(ename)
+
+    app.run(host=ehost, port=eport)
